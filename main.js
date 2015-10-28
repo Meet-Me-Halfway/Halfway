04f1f8fc260497b4a52c200c53a9b2e2aaca921b
Roxanne Ravago <roxanne.ravago@gmail.com>
2015-10-27 22:35:47 -0400

    Organized app structure, added basic styling, initialized map, added click handler function, added getLocationMidpoint function

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-// gets location1 input on click event for location1_btn
-// gets location2 input on click event for location2_btn
+// gets location1 and location2 input on click event for submit button
+$( "#submit" ).click(function(e) {
+  var cities = [ $("#location1").val(), $("#location2").val() ];
+​
+  cities = cities.map(function(city) {
+    return encodeURI(city.toLowerCase().trim());
+  });
+​
+  console.log(cities);
 
-// make get request for google maps
-// initialize google maps on map canvas 
+ // TO DO: convert into cities into lat lng coordinates
+});
 
-// uses location1 input and adds marker on map of location1
-// uses location2 input and adds marker on map of location2 
-// uses location1 and location2 inputs to calculate its midpoint and adds marker on map of location3
+function getLocationCoordinates(city){
+  // TO DO: get Google Maps Geocoding service 
+  // to convert address to location coordinates
+}
+
+function addLocationMarkers(){
+  // TO DO: find out from Google Maps API docs how to add marker on map
+  // TO DO: add markers on map of location1 and location2 coordinates
+}
+
+// uses location1 and location2 inputs to calculate its midpoint 
+// and adds marker on map of location3 coordinates
+function getMidpointLocation(start, end){
+  var lat_midpoint = (end.lat + start.lat) / 2;
+  var long_midpoint = (end.long + start.long) / 2;
+  return {
+    lat: lat_midpoint,
+    long: long_midpoint
+  } 
+}
+
+
+
+
+ 
