e25daf1cd28f17639aaf6501ecd904bf26aa74f2
Matt Cunningham <MSC86@users.noreply.github.com>
2015-10-27 20:02:50 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// gets location1 input on click event for location1_btn
-// gets location2 input on click event for location2_btn
-
-// make get request for google maps
-// initialize google maps on map canvas 
-
-// uses location1 input and adds marker on map of location1
-// uses location2 input and adds marker on map of location2 
-// uses location1 and location2 inputs to calculate its midpoint and adds marker on map of location3
